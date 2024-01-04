fa3f2ac9a24e8cbedec2190d9c5c41017b6d0663
Brain-Effects <braineffects@github.com>
2024-01-04 03:18:09 -0800

    Positive anything is better than negative nothing

---
+++ 0-positive_or_negative.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10, 10)
+
+# Check if the number is positive, zero, or negative and print the result in a single line
+if number > 0:
+    print(f"{number} is positive")
+elif number == 0:
+    print(f"{number} is zero")
+else:
+    print(f"{number} is negative")
