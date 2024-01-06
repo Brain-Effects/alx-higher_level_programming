7987877feaa3f9bf9c87a71963ae9f324c4bc0d8
Brain-Effects <braineffects@github.com>
2024-01-06 03:03:02 -0800

    Lists of lists = Matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    print('\n'.join(' '.join("{:d}".format(num) for num in row) for row in matrix))
+    for row in matrix:
+        row_str = ' '.join("{:d}".format(num) for num in row)
+        print(row_str)
