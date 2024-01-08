8e8d74e870a5bb83cae959d58f0a228dfa2501f8
Brain-Effects <braineffects@github.com>
2024-01-08 02:11:07 -0800

    To uppercase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def uppercase(str):
+    result = ""
+    for c in str:
+        if 97 <= ord(c) <= 122:  # ASCII values for lowercase letters
+            result += chr(ord(c) - 32)  # Convert to uppercase
+        else:
+            result += c
+    print(result)
+    print()
