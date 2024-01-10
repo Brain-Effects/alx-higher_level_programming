46536c8f4d827ebbc837de493285807d14c1bcad
Brain-Effects <braineffects@github.com>
2024-01-10 03:30:05 -0800

    Only differents

---
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def only_diff_elements(set_1, set_2):
+    # Use the symmetric_difference method of set
+    #to find elements present in only one set
+    only_diff_set = set_1.symmetric_difference(set_2)
+
+    # Return the set of elements present in only one set
+    return only_diff_set
