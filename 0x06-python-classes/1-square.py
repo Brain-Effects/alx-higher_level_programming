37ce10f295c891b47699388c9a057766fe1d412c
Brain-Effects <braineffects@github.com>
2024-01-23 00:43:51 -0800

    Square with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+This module defines a Square class.
+
+The purpose of this module is to provide a simple representation of a square.
+"""
+
+
+class Square:
+    """
+    This is the Square class.
+
+    The purpose of this class is to define a square with a specific size.
+    """
+
+    def __init__(self, size):
+        """
+        This is the constructor method for the Square class.
+
+        It initializes a Square instance with a given size.
+        """
+        self.__size = size
