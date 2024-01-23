e276b0dded14734cf50582e1ef8e5599784affcd
Brain-Effects <braineffects@github.com>
2024-01-23 00:33:05 -0800

    My first square

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+This module defines a Square class.
+
+The purpose of this module is to provide a simple representation of a square.
+"""
+
+
+class Square:
+    """
+    This is the Square class.
+
+    The purpose of this class is to define a square. Currently,
+    it's an empty class.
+    """
+    pass
