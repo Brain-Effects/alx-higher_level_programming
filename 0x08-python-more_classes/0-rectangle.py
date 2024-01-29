642755aeac29c868a495601eb459fbbd89880ee6
Brain-Effects <braineffects@github.com>
2024-01-29 03:30:44 -0800

    Simple rectangle

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,3 +1,15 @@
 #!/usr/bin/python3
 class Rectangle:
+    """
+    This is an empty class named Rectangle.
+
+    Currently, this class doesn't have any attributes or methods.
+    It serves as a blueprint for creating Rectangle objects in the future.
+
+    Attributes:
+    None
+
+    Methods:
+    None
+    """
     pass
