02652552eb6e45f3bc10417bab001179530ebdf3
Brain-Effects <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@github.com>
2024-02-02 17:57:29 -0800

    Divide a matrix

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -15,33 +15,16 @@ def matrix_divided(matrix, div):
        not all(isinstance(row, list) for row in matrix) or \
        not \
        all(isinstance(item, (int, float)) for row in matrix for item in row):
-
-        print("matrix must be a matrix (list of lists) of integers/floats")
-        return
+        raise TypeError("matrix must be a matrix (list of lists)\
+                of integers/floats")
 
     if not all(len(row) == len(matrix[0]) for row in matrix):
-        print("Each row of the matrix must have the same size")
-        return
+        raise TypeError("Each row of the matrix must have the same size")
 
     if not isinstance(div, (int, float)):
-        print("div must be a number")
-        return
+        raise TypeError("div must be a number")
 
     if div == 0:
-        print("division by zero")
-        return
+        raise ZeroDivisionError("division by zero")
 
     return [[round(item / div, 2) for item in row] for row in matrix]
-
-
-# Test cases
-print(matrix_divided([[1, 2, 3], [4, 5, 6]], 2))
-# Expected: [[0.5, 1.0, 1.5], [2.0, 2.5, 3.0]]
-print(matrix_divided("invalid", 2))
-# Expected: "matrix must be a matrix (list of lists) of integers/floats"
-print(matrix_divided([[1, 2, 3], [4, 5]], 2))
-# Expected: "Each row of the matrix must have the same size"
-print(matrix_divided([[1, 2, 3], [4, 5, 6]], "invalid"))
-# Expected: "div must be a number"
-print(matrix_divided([[1, 2, 3], [4, 5, 6]], 0))
-# Expected: "division by zero"
