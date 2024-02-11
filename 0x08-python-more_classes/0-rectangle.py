951581e2fb89affc4c2010279508d66d8a40232d
Brain-Effects <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@github.com>
2024-02-11 01:47:51 -0800

    Simple rectangle

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
 # Define a class Rectangle
 class Rectangle:
-    """
-    This is an empty class that defines a rectangle.
-    
-    It currently does not have any attributes or methods.
-    Future implementations can include attributes such as length and width,
-    and methods to calculate the area and perimeter of the rectangle.
+    """This class represents a rectangle shape.
+
+    It does not have any attributes or methods yet, but they can be added later.
     """
     # Use the pass statement to indicate an empty class
     pass
