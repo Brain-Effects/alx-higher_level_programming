3174dc527a4076de05efb324ea5b4ed92d446bc7
Brain-Effects <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@github.com>
2024-02-11 01:53:03 -0800

    Simple rectangle

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,9 +1,16 @@
 #!/usr/bin/python3
+"""
+This module contains the Rectangle class and its methods.
+
+The Rectangle class represents a rectangle shape and can be used
+for various purposes such as drawing, calculating area and perimeter, etc.
+"""
 # Define a class Rectangle
 class Rectangle:
     """This class represents a rectangle shape.
 
-    It does not have any attributes or methods yet, but they can be added later.
+    It does not have any attributes or methods yet, but they can
+    be added later.
     """
     # Use the pass statement to indicate an empty class
     pass
