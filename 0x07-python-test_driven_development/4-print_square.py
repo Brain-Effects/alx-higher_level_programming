4f935f9429ccf2ad9ddfe543a668c692ec614753
Brain-Effects <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@github.com>
2024-02-11 05:55:43 -0800

    Print square

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+
+def print_square(size):
+    """
+    Prints a square with the character #.
+
+    Args:
+        size (int): The size length of the square.
+
+    Raises:
+        TypeError: If size is not an integer, or if size is a
+        float and is less than 0.
+        ValueError: If size is less than 0.
+    """
+
+    # Check if size is an integer
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+
+    # Check if size is positive
+    if size < 0:
+        raise ValueError("size must be >= 0")
+
+    # Print the square
+    for i in range(size):
+        print("#" * size)
