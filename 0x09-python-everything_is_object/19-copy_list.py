709ec1512a8103cb87537c68aed6215878680217
Brain-Effects <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@github.com>
2024-03-02 03:35:27 -0800

    Clear strings

---
+++ 0x09-python-everything_is_object/19-copy_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""
+    This function uses slicing to create and return
+    a copy of the input list l. The slice [:] creates a new list that
+    contains all elements of the original list.
+"""
+def copy_list(l):
+    return l[:]
