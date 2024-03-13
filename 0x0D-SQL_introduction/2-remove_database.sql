be039919ac4a447e801afa7dabaa173f1b3f4cbc
Brain-Effects <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@github.coom>
2024-03-13 03:04:24 -0700

    Delete a database

---
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -0,0 +1,4 @@
+-- This script deletes the database hbtn_0c_0 in your MySQL server
+-- If the database hbtn_0c_0 doesn't exist, it will not fail
+DROP DATABASE IF EXISTS hbtn_0c_0;
+-- End of script
