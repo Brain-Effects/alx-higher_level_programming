1844865244d36d19d917f3c4a4cb6dae0d8753e6
Brain-Effects <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@github.coom>
2024-03-13 02:53:25 -0700

    List databases

--- 0x0D-SQL_introduction/0-list_databases.sql
+++ 0x0D-SQL_introduction/0-list_databases.sql
@@ -1,26 +1,3 @@
-#!/usr/bin/python3
-import mysql.connector
-
-# Establish the connection
-cnx = mysql.connector.connect(
-  host="localhost",
-  user="Brain-Effects",
-  password="BraEff9003"
-)
-
-# Create a cursor object
-cursor = cnx.cursor()
-
-# Execute the SQL command
-cursor.execute("SHOW DATABASES")
-
-# Fetch all the rows
-databases = cursor.fetchall()
-
-# Print all databases
-for database in databases:
-  print(database[0])
-
-# Close the cursor and connection
-cursor.close()
-cnx.close()
+-- This script lists all databases of your MySQL server
+SHOW DATABASES;
+-- End of script
