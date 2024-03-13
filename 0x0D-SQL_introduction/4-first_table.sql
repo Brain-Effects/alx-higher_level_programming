98720a21b80a125f561158079cc3cd6738044911
Brain-Effects <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@github.coom>
2024-03-13 03:20:48 -0700

    First table

---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,4 @@
+-- This script creates a table called first_table in the current database in
+-- your MySQL server If the table first_table already exists, it will not fail
+CREATE TABLE IF NOT EXISTS first_table ( id INT, name VARCHAR(256));
+-- End of script
