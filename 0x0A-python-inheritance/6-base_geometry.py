437c6e6ebe6878e499849bce1454538392389f05
Brain-Effects <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@github.coom>
2024-03-16 05:49:06 -0700

    Improve Geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+This module contains a class BaseGeometry with a public instance method.
+"""
+
+class BaseGeometry:
+    """
+    This is a class named BaseGeometry that includes a public instance method.
+    """
+
+    def area(self):
+        """
+        This is a public instance method that raises an Exception with a message.
+        """
+        raise Exception("area() is not implemented")
