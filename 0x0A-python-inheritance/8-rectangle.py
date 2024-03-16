b80e22cff9cc9968f807ddc9978b95d53a514850
Brain-Effects <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@github.coom>
2024-03-16 06:14:26 -0700

    Rectangle

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,11 +1,17 @@
 #!/usr/bin/python3
-"""Module for Rectangle class"""
+"""
+Module for Rectangle class
+"""
 
 
 class BaseGeometry:
-    """BaseGeometry class"""
+    """
+    BaseGeometry class
+    """
     def integer_validator(self, name, value):
-        """Validates value"""
+        """
+        Validates value
+        """
         if type(value) is not int:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
@@ -13,10 +19,20 @@ class BaseGeometry:
 
 
 class Rectangle(BaseGeometry):
-    """Rectangle class that inherits from BaseGeometry"""
+    """
+    Rectangle class that inherits from BaseGeometry
+    """
     def __init__(self, width, height):
-        """Instantiation with width and height"""
+        """
+        Instantiation with width and height
+        """
         self.integer_validator("width", width)
         self.integer_validator("height", height)
         self.__width = width
         self.__height = height
+
+    def area(self):
+        """
+        Returns the area of the rectangle
+        """
+        return self.__width * self.__height
