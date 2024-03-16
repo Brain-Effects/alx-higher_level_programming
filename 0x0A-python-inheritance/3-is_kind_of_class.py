97ad1b05ce8365344590f7a33aff37c367c7ee69
Brain-Effects <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@github.coom>
2024-03-16 05:36:32 -0700

    Same class or inherit from

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
 """
 This module contains a function that checks if an object is an instance of,
-or if the object is an instance of a class that inherited from, the specified class.
+or if the object is an instance of a class that inherited from,
+the specified class.
 """
 
 
 def is_kind_of_class(obj, a_class):
     """
-    Function to check if an object is an instance of, or if the object is an instance
+    Function to check if an object is an instance of, or if the
+    object is an instance
     of a class that inherited from, the specified class.
 
     Args:
@@ -15,7 +17,8 @@ def is_kind_of_class(obj, a_class):
         a_class: The class to check against.
 
     Returns:
-        True if the object is an instance of, or if the object is an instance of a class
+        True if the object is an instance of, or if the object is
+        an instance of a class
         that inherited from, the specified class. False otherwise.
     """
     return isinstance(obj, a_class)
