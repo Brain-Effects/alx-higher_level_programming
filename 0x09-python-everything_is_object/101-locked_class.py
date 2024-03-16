9ad583f22fdd753e202ede9e489521840f0206b9
Brain-Effects <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@github.coom>
2024-03-16 02:05:07 -0700

    Low memory cost

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
 class LockedClass:
+    """
+    LockedClass only allows creating the 'first_name' instance attribute.
+    Attempting to create any other attribute will result in an AttributeError.
+    """
     __slots__ = ['first_name']
