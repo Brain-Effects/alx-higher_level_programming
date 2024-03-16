c7018c57c10057e612b4f12026d2f5f6d230a9bf
Brain-Effects <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@github.coom>
2024-03-16 01:31:34 -0700

    Copy a list object

--- 0x09-python-everything_is_object/19-copy_list.py
+++ 0x09-python-everything_is_object/19-copy_list.py
@@ -1,8 +1,3 @@
 #!/usr/bin/python3
-"""
-    This function uses slicing to create and return
-    a copy of the input list l. The slice [:] creates a new list that
-    contains all elements of the original list.
-"""
 def copy_list(l):
     return l[:]
