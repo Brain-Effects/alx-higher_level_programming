1cc299fd2e30687ebb56631563950504a55d5cd8
Brain-Effects <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@github.coom>
2024-03-17 04:54:48 -0700

    My privileges!

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,9 +1,5 @@
--- Check if user_0d_1 exists and list privileges
-SELECT User, Host, Select_priv, Insert_priv, Update_priv, Delete_priv, Create_priv, Drop_priv
-FROM mysql.user 
-WHERE User='user_0d_1';
+-- For user_0d_1
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
 
--- Check if user_0d_2 exists and list privileges
-SELECT User, Host, Select_priv, Insert_priv, Update_priv, Delete_priv, Create_priv, Drop_priv
-FROM mysql.user 
-WHERE User='user_0d_2';
+-- For user_0d_2
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
