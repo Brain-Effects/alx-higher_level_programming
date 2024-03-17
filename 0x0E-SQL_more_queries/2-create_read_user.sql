1808dd230638d24d194175d7d46d99297083a0d5
Brain-Effects <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@github.coom>
2024-03-17 05:02:49 -0700

    Read user

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,11 @@
+-- Create database if not exists
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+
+-- Create user if not exists
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+
+-- Grant SELECT privilege on hbtn_0d_2 to user_0d_2
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
+
+-- Flush privileges to make the changes take effect
+FLUSH PRIVILEGES;
