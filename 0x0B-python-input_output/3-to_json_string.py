3dc6462e603c703a30d3f5851a42fdde967287c7
Brain-Effects <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@github.coom>
2024-03-17 09:53:51 -0700

    To JSON string

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+This module contains a function that returns the JSON representation
+of an object (string).
+
+The function does not handle exceptions if the object can't be serialized.
+No modules are imported for this function.
+"""
+
+
+def to_json_string(my_obj):
+    """
+    Function to return the JSON representation of an object.
+
+    Args:
+        my_obj: The object to be serialized.
+
+    Returns:
+        str: The JSON representation of the object.
+    """
+    return str(my_obj).replace("'", '"')
