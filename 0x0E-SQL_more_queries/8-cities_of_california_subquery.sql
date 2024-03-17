d770ae225872ac2bf538915418ff7743b7c80e03
Brain-Effects <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@github.coom>
2024-03-17 06:10:43 -0700

    Cities of California

---
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,5 @@
+-- Select the id of California from the states table
+SET @california_id = (SELECT id FROM states WHERE name = 'California');
+
+-- Select all cities in California from the cities table
+SELECT * FROM cities WHERE state_id = @california_id ORDER BY id ASC;
