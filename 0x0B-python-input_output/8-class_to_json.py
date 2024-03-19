c68d77d77d813f61f9e2705f39f4194f25104ce0
Brain-Effects <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@github.coom>
2024-03-19 01:36:50 -0700

    Class to JSON

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,4 +1,20 @@
 #!/usr/bin/python3
+"""
+This module provides a function to convert an object's attributes
+into a dictionary with simple data structures for JSON serialization.
+
+Functions:
+- class_to_json(obj): Returns the dictionary description with
+simple data structure for JSON serialization of an object.
+
+The function in this module takes an instance of a class as
+input and returns a dictionary that represents the object's attributes.
+The attributes are serialized into simple data structures: list,
+dictionary, string, integer, and boolean. This function does not
+handle exceptions if the object can't be serialized.
+"""
+
+
 def class_to_json(obj):
     """
     This function returns the dictionary description with simple
