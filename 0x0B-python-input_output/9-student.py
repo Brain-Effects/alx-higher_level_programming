16ad940e7c38e9babb08fdc58151f5fe5dd0c237
Brain-Effects <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@github.coom>
2024-03-19 01:47:00 -0700

    Student to JSON

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -3,20 +3,21 @@
 
 class Student:
     """
-    This class defines a student by their first name, last name, and age.
+    `Student` is a class that defines a student by their
+    first name, last name, and age.
 
-    Public instance attributes:
-    - first_name
-    - last_name
-    - age
+    Attributes:
+    first_name (str): The first name of the student.
+    last_name (str): The last name of the student.
+    age (int): The age of the student.
 
-    The class provides a method to retrieve a dictionary
+    The class provides a method `to_json` to retrieve a dictionary
     representation of a Student instance.
     """
 
     def __init__(self, first_name, last_name, age):
         """
-        This method initializes a Student instance.
+        Initializes a Student instance with first name, last name, and age.
 
         Parameters:
         first_name (str): The first name of the student.
@@ -29,10 +30,10 @@ class Student:
 
     def to_json(self):
         """
-        This method retrieves a dictionary representation of a
-        Student instance.
+        Retrieves a dictionary representation of a Student instance.
 
         Returns:
-        dict: A dictionary that represents the Student instance.
+        dict: A dictionary that represents the Student instance with
+        keys as attribute names and values as attribute values.
         """
         return self.__dict__
