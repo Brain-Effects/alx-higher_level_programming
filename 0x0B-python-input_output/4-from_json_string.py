f6e49973ea86fff399d6f7a49eeed0f161e5df29
Brain-Effects <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@github.coom>
2024-03-19 00:55:45 -0700

    From JSON string to Object

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """
-The `json` module is a part of the standard library in Python. It provides methods to parse JSON strings and convert Python data structures to JSON format.
+The `json` module is a part of the standard library in Python.
+It provides methods to parse JSON strings and convert Python data
+structures to JSON format.
 """
 import json
 
@@ -16,7 +18,9 @@ def from_json_string(my_str):
     object: A Python object that represents the JSON string.
 
     Note:
-    This function does not handle exceptions if the JSON string does not represent a valid object.
-    If you pass an invalid JSON string to this function, it will raise a json.JSONDecodeError.
+    This function does not handle exceptions if the JSON string does not
+    represent a valid object.
+    If you pass an invalid JSON string to this function, it will
+    raise a json.JSONDecodeError.
     """
     return json.loads(my_str)
