5f5b95d80e2a7b1542969da08606b74cecd28aa5
Brain-Effects <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@github.coom>
2024-04-14 13:12:24 -0700

    First state model

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -2,7 +2,7 @@
 """
     This class inherits from Base and links to the MySQL table states.
 """
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, create_engine
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
@@ -11,3 +11,7 @@ class State(Base):
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
+
+engine = create_engine('mysql+mysqldb://username:password@localhost:3306/database_name?charset=latin1')
+
+Base.metadata.create_all(engine)
