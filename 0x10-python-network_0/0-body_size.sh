685fefa9d75a9561e1ccec4ed62dfc0c4f856e15
Brain-Effects <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@github.coom>
2024-04-27 22:58:50 -0700

    cURL body size

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
-# This script sends a request to a URL and displays the size of the
-# body of the response
+# This script sends a request to a URL and displays the size of the body of the response
 
 url=$1
-response_body=$(curl -s "$url")
-response_size=$(echo -n "$response_body" | wc -c)
-echo "$response_size"
+curl -s "$url" -o /dev/null -w '%{size_download}\n'
