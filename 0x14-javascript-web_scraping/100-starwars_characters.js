5bc0d92b279c5f77979284421210fe74ff12c10b
Brain-Effects <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@github.com>
2024-05-21 15:36:10 -0700

    Who was playing in this movie?

---
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,26 @@
+#!/usr/bin/node
+const request = require('request');
+const movieId = process.argv[2];
+const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+
+request(url, function (error, response, body) {
+  if (error) {
+    console.error('error:', error);
+    return;
+  }
+
+  const movie = JSON.parse(body);
+  const characterUrls = movie.characters;
+
+  characterUrls.forEach(url => {
+    request(url, function (error, response, body) {
+      if (error) {
+        console.error('error:', error);
+        return;
+      }
+
+      const character = JSON.parse(body);
+      console.log(character.name);
+    });
+  });
+});
